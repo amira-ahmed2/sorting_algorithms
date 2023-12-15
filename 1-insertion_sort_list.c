57d290ca846e856bf902b90c38ec4845b6cae1e2
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-12-15 17:22:45 +0200

    add

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * swap_two_num - Swap two integers in an array.
+ * @n1: The first integer to swap.
+ * @n2: The second integer to swap.
+ */
+void swap_node(int *n1, int *n2)
+{
+
+}
+
+/**
+ * insertion_sort_list - insertion_sort_list
+ *
+ * @list: The doubly-linked list of integers
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+}
