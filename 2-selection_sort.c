97b1802ba8bb626bbebb93ff765d74a911513552
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-12-16 14:19:07 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,7 +6,8 @@
  * @n1: The first integer to swap.
  * @n2: The second integer to swap.
  */
-void swap_two(int *n1, int *n2) {
+void swap_two(int *n1, int *n2)
+{
 	int temp = *n1;
 	*n1 = *n2;
 	*n2 = temp;
