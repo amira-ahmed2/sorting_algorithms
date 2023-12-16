f3ad6506d3a8c3c7574481d021899db2aa53392e
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-12-16 14:47:04 +0200

    up

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,22 +23,24 @@ void swap_ints(int *n1, int *n2)
 void selection_sort(int *array, size_t size)
 {
 
-	int *minpos;
-	size_t i, j;
-
+	int i, j, minpos;
+	
 	if (array == NULL || size < 2)
 		return;
-
+	
 	for (i = 0; i < size - 1; i++)
 	{
-		minpos = array + i;
-		for (j = i + 1; j < size; j++)
-			minpos = (array[j] < *minpos) ? (array + j) : minpos;
-
-		if ((array + i) != minpos)
+		minpos = i;
+		for (j = i + 1; j <size; j++)
 		{
-			swap_ints(array + i, minpos);
+			if (array[j] < array[minpos])
+				minpos = j;
+		}
+		if (minpos != i)
+		{
+			swap_two(array + i, array + minpos);
 			print_array(array, size);
+
 		}
 	}
 }
