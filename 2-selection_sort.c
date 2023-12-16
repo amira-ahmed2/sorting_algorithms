e908d99fba40e35ec068afdfa7d6a5c4945dc42c
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-12-16 20:17:08 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap_ints - Swap two integers in an array.
+ * @n1: The first integer to swap.
+ * @n2: The second integer to swap.
+ */
+void swap_ints(int *n1, int *n2)
+{
+	int temp;
+
+	temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
+}
+
+/**
  * selection_sort - selection sort
  *
  * @array: The array
