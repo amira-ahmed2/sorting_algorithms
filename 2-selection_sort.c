0b6a96220152c25d3c79e706352e5725578ce628
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-12-16 14:35:03 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,41 +1,44 @@
 #include "sort.h"
 
-
 /**
- * swap_selection_sort - Swap two integers in an array.
- * @n1: The first integer to swap.
- * @n2: The second integer to swap.
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-void swap_selection_sort(int *n1, int *n2)
+void swap_ints(int *a, int *b)
 {
-	int temp = *n1;
-	*n1 = *n2;
-	*n2 = temp;
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
- * selection_sort - selection sort
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
  *
- * @array: The array
- * @size: size is array
+ * Description: Prints the array after each swap.
  */
 void selection_sort(int *array, size_t size)
-{	
-	int *min_postion;
-	int i, j;
+{
+	int *min;
+	size_t i, j;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_postion = array + i;
+		min = array + i;
 		for (j = i + 1; j < size; j++)
-			min = (array[j] < *min_postion) ? (array + j) : min_postion;
+			min = (array[j] < *min) ? (array + j) : min;
 
-		if ((array + i) != min_postion)
+		if ((array + i) != min)
 		{
-			swap_selection_sort(array + i, min_postion);
+			swap_ints(array + i, min);
 			print_array(array, size);
 		}
 	}
