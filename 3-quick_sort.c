9f78f583a9855e23b9e4b136e9b0f2ceddd05ebf
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-12-16 20:29:37 +0200

    Create 3-quick_sort.c

---
+++ 3-quick_sort.c
@@ -0,0 +1,91 @@
+#include "sort.h"
+
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @n1: The first integer to swap.
+ * @n2: The second integer to swap.
+ */
+void swap_ints(int *n1, int *n2)
+{
+	int temp;
+
+	temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
+}
+
+/**
+ * lomuto_partition - Order a subset of an array of integers according to
+ *                    last element as pivot
+ * @array: The array
+ * @size: size is array
+ * @left: The starting index
+ * @right: The ending index
+ *
+ * Return: final index.
+ */
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+	int *pivot, above, below;
+
+	pivot = array + right;
+	for (above = below = left; below < right; below++)
+	{
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				swap_ints(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
+	}
+
+	if (array[above] > *pivot)
+	{
+		swap_ints(array + above, pivot);
+		print_array(array, size);
+	}
+
+	return (above);
+}
+
+/**
+ * lomuto_sort - Implement the quicksort
+ * @array: The array
+ * @size: size is array
+ * @left: The starting index of the array
+ * @right: The ending index of the array
+ *
+ */
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
+
+/**
+ * quick_sort - quick_sort
+ *
+ * @array: The array
+ * @size: size is array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
